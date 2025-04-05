5d702ca26c4ea42bc78f1ba5134eeba6d30c70a9
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2025-04-05 13:39:32 -0700

    deploying

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ mongoose
 
 const app = express();
 
-
+// Middleware to parse incoming requests
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
@@ -19,20 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "https://techzone-eight.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "https://techzone-eight.vercel.app", 
+    credentials: true,  
+    methods: ["GET", "POST", "PUT", "DELETE"],  
+    allowedHeaders: ["Content-Type", "Authorization"], 
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://techzone-eight.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use((req, res, next) => {
   console.log("Incoming Request:", req.method, req.url);
@@ -44,6 +37,7 @@ app.use((req, res, next) => {
 app.use("/api/payments", require("./routes/paymentRoutes"));
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/webhookRoutes")); 
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
