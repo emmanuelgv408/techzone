e6a1f7ccf0aa04f4600924df5d6add1aa416cf58
Emmanuel Guerrero Vega <emmanuelgv@live.com>
2025-04-12 13:36:23 -0700

    adding spinners for loading states

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ShopContext } from '../context/ShopContext';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { FaSpinner } from "react-icons/fa"; 
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: 'staysi6@gmail.com',
-    password: '1234567',
+    email: "staysi6@gmail.com",
+    password: "1234567",
   });
   const navigate = useNavigate();
-  const {loginUser} = useContext(ShopContext)
+  const { loginUser } = useContext(ShopContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    setIsLoading(true);
 
     const success = await loginUser(formData.email, formData.password);
     if (success) {
-      navigate('/'); 
+      navigate("/");
     }
+    setIsLoading(false);
   };
-
-  
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md px-3 py-10 rounded-lg flex flex-col gap-3 bg-gray bg-opacity-50 backdrop-blur-lg">
-        <h2 className='uppercase tracking-wider text-3xl font-semibold text-center'>TechZone</h2>
+        <h2 className="uppercase tracking-wider text-3xl font-semibold text-center">
+          TechZone
+        </h2>
         <h2 className="text-xl font-semibold">Login</h2>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
@@ -34,7 +37,9 @@ const Login = () => {
             name="email"
             placeholder="Email"
             value={formData.email}
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
             className="border px-2 border-black/25 shadow-sm rounded placeholder-black"
           />
           <input
@@ -42,25 +47,29 @@ const Login = () => {
             name="password"
             placeholder="Password"
             value={formData.password}
-            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
             className="border px-2 border-black/25 shadow-sm rounded placeholder-black"
           />
           <button
             type="submit"
-            className="bg-tan hover:bg-tan/90 text-white py-2 rounded-md "
+            className="bg-tan hover:bg-tan/90 text-white py-2 rounded-md flex items-center justify-center"
+            disabled={isLoading}
           >
-            Continue
+            {isLoading ? (
+              <FaSpinner className="animate-spin" />
+            ) : (
+              "Continue"
+            )}
           </button>
         </form>
 
         {/* Register Link */}
         <div className="text-center mt-4">
           <span className="text-sm text-gray-600">
-            Don't have an account?{' '}
-            <a
-              href="/register"
-              className="text-blue-500 hover:underline"
-            >
+            Don't have an account?{" "}
+            <a href="/register" className="text-blue-500 hover:underline">
               Register
             </a>
           </span>
